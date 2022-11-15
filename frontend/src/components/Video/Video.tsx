f898d3fd7e19acd75378a96685d121ceb327ab90
seyoung8239 <seyoung8239@gmail.com>
2022-11-15 15:05:08 +0900

    feat: Video 컴포넌트 추가

--- frontend/src/components/Video/Video.tsx
+++ frontend/src/components/Video/Video.tsx
@@ -1,7 +1,31 @@
-import React, { useRef } from 'react';
+import React, { useRef, Dispatch, SetStateAction, useEffect } from 'react';
 
-function Video() {
-	const videoRef = useRef();
+interface PropType {
+	currentTime: number;
+	setCurrentTime: Dispatch<SetStateAction<number>>;
+	isFbClicked: boolean;
+	setIsFbClicked: Dispatch<SetStateAction<boolean>>;
+}
+
+function Video({ currentTime, setCurrentTime, isFbClicked, setIsFbClicked }: PropType) {
+	const videoRef = useRef() as React.MutableRefObject<HTMLVideoElement>;
+	const aSec = 1000;
+
+	useEffect(() => {
+		const interverId = setInterval(() => {
+			setCurrentTime(videoRef.current.currentTime);
+		}, aSec);
+
+		return () => clearInterval(interverId);
+	}, [setCurrentTime]);
+
+	useEffect(() => {
+		if (isFbClicked) {
+			videoRef.current.currentTime = currentTime;
+			setIsFbClicked(false);
+		}
+	}, [currentTime]);
+
 	return (
 		<div>
 			<video width="400px" src="test.mov" controls autoPlay ref={videoRef}></video>
