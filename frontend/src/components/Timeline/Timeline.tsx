7df8ffabc8d6aaffe830d936ba64c48fd760efe4
seyoung8239 <seyoung8239@gmail.com>
2022-11-15 21:25:29 +0900

    feat: timeline 구성 로직 추가

--- frontend/src/components/Timeline/Timeline.tsx
+++ frontend/src/components/Timeline/Timeline.tsx
@@ -1,10 +1,18 @@
-import React, { Dispatch, SetStateAction, useCallback, useState } from 'react';
+import React, { Dispatch, SetStateAction, useCallback, useEffect, useState } from 'react';
+import Input from '@components/Input/Input';
+import { lowerBoundFB } from '../../utils/utils';
+import { FeedbackType } from '@types/common';
 
 const initialTimeline = [
-	{ id: '1', startTime: 1, endTime: 3, content: 'hello' },
-	{ id: '2', startTime: 4, endTime: 6, content: 'hello' },
-	{ id: '3', startTime: 9, endTime: 11, content: 'hello' },
-	{ id: '4', startTime: 13, endTime: 14, content: 'hello' },
+	{ id: '1', startTime: 1, endTime: 2, content: 'hello' },
+	{ id: '2', startTime: 2, endTime: 3, content: 'hello' },
+	{ id: '3', startTime: 3, endTime: 4, content: 'hello' },
+	{ id: '4', startTime: 5, endTime: 6, content: 'hello' },
+	{ id: '5', startTime: 6, endTime: 7, content: 'hello' },
+	{ id: '6', startTime: 8, endTime: 9, content: 'hello' },
+	{ id: '7', startTime: 10, endTime: 11, content: 'hello' },
+	{ id: '8', startTime: 11, endTime: 12, content: 'hello' },
+	{ id: '9', startTime: 13, endTime: 14, content: 'hello' },
 ];
 interface PropType {
 	currentTime: number;
@@ -12,8 +20,14 @@ interface PropType {
 	setIsFbClicked: Dispatch<SetStateAction<boolean>>;
 }
 
+const timeLineSize = 4;
+const firstIndex = 0;
+const prevFbNum = 1;
+const nextFbNum = 2;
+
 function Timeline({ currentTime, setCurrentTime, setIsFbClicked }: PropType) {
-	const [timeline, setTimeline] = useState(initialTimeline);
+	const [feedbackBoxes, setFeedbackBoxes] = useState<FeedbackType[]>(initialTimeline);
+	const [timeline, setTimeline] = useState<FeedbackType[]>([]);
 
 	const handleClickFeedbackBox = useCallback(
 		(nextTime: number) => {
@@ -22,6 +36,33 @@ function Timeline({ currentTime, setCurrentTime, setIsFbClicked }: PropType) {
 		},
 		[setCurrentTime]
 	);
+
+	useEffect(() => {
+		const orderedFbs = feedbackBoxes.sort(fbCmp);
+		const fbsLen = orderedFbs.length;
+		(function () {
+			if (fbsLen <= timeLineSize) {
+				setTimeline(orderedFbs);
+				return;
+			}
+
+			const curFbIdx = lowerBoundFB(orderedFbs, currentTime);
+			const lowIdxBound = firstIndex + prevFbNum;
+			const hightIdxBound = fbsLen - 1 - nextFbNum;
+			const leftOffset = curFbIdx < lowIdxBound ? lowIdxBound - curFbIdx : 0;
+			const rightOffset = curFbIdx > hightIdxBound ? curFbIdx - hightIdxBound : 0;
+
+			const newTimeline = orderedFbs.slice(
+				curFbIdx - prevFbNum - rightOffset,
+				curFbIdx + nextFbNum + leftOffset + 1
+			);
+			setTimeline(newTimeline);
+		})();
+	}, [currentTime, feedbackBoxes]);
+
+	const fbCmp = (a: FeedbackType, b: FeedbackType) => {
+		return a.startTime - b.startTime;
+	};
 
 	return (
 		<div>
