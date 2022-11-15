bb7fea2c9f4bb8283bb8655f172eb4c346f9a163
seyoung8239 <seyoung8239@gmail.com>
2022-11-15 21:25:12 +0900

    feat: lowbound util함수 추가

---
+++ frontend/src/utils/utils.ts
@@ -0,0 +1,20 @@
+import { FeedbackType } from '@types/common';
+
+export const lowerBoundFB = (list: Array<FeedbackType>, key: number) => {
+	let mid;
+	let start = 0;
+	let end = list.length - 1;
+	let result = end;
+
+	while (start <= end) {
+		mid = Math.floor((start + end) / 2);
+		if (list[mid].startTime < key) {
+			start = mid + 1;
+			continue;
+		}
+		result = Math.min(result, mid);
+		end = mid - 1;
+	}
+
+	return result;
+};
